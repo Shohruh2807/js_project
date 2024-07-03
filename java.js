1d975e1e34cdfb89ea0aaaaf1df19566333390c5
Shohruh2807 <jalilovshohruh2010@gmail.com>
2024-07-04 01:53:23 +0500

    javascript_project

---
+++ java.js
@@ -0,0 +1,45 @@
+'use strict'
+let numberOfMovies= +prompt('Nechta kino ko’rdingiz?')
+const names={
+}
+const actors={
+
+}
+const genres=[]
+const moveisDB={
+    numberOfMovies,
+    name,
+    actors,
+    genres,
+    private: false
+}
+let firstQuestion1=prompt("Oxirgi ko’rgan filmingiz?"),
+firstQuestion2=+prompt("Nechi baxo berasiz?"),
+firstQuestion3=prompt("Oxirgi ko’rgan filmingiz?"),
+firstQuestion4=+prompt("Nechi baxo berasiz?"),
+secondQuestion1=prompt("Sevimli aktyoringiz?"),
+secondQuestion2=prompt("Sevimli aktrissangiz?"),
+thirdQuestion1=prompt("Yoqtirgan Janringiz?"),
+thirdQuestion2=prompt("Yoqtirgan Multfilmingiz?"),
+thirdQuestion3=prompt("Yoqtirgan Janringiz?"),
+thirdQuestion4=prompt("Yoqtirgan Multfilmingiz?"),
+thirdQuestion5=prompt("Yoqtirgan Janringiz?"),
+thirdQuestion6=prompt("Yoqtirgan Multfilmingiz?")
+const movies={
+    Oppenheimer: 10,
+    Barbie: 8,
+    numberOfMovies,
+    firstQuestion1,
+    firstQuestion2,
+    firstQuestion3,
+    firstQuestion4,
+    secondQuestion1,
+    secondQuestion2,
+    thirdQuestion1,
+    thirdQuestion2,
+    thirdQuestion3,
+    thirdQuestion4,
+    thirdQuestion5,
+    thirdQuestion6
+}
+console.log(movies)
